672fca1abe92428b5b92452ad80a171b00a70dfe
GrantSRechtin <gsullivanr@icloud.com>
2026-03-11 16:32:47 -0400

    .

--- Rocky_67.m
+++ Rocky_67.m
@@ -52,7 +52,7 @@ grid on
 mask = (t >= 5) & (t <= 25) & (theta <= -2);
 
 t_sel = t(mask); t_sel = t_sel   - t_sel(1);
-theta_sel = y(mask);
+theta_sel = theta(mask);
 
 figure(4);
 clf(4);
@@ -62,19 +62,48 @@ ylabel('theta (radians)')
 title('cropped gyro data')
 grid on
 
-[~,locs] = findpeaks(theta_sel, t_sel, 'MinPeakProminence', 0.12);
+[pks,locs] = findpeaks(theta_sel, t_sel, 'MinPeakProminence', 0.12);
 periods = diff(locs);
 periods = periods(isfinite(periods) & periods>0);
-fn_time = (2*pi)/mean(periods);
+omega_d = (2*pi)/mean(periods);
 
-length = 9.8/(fn_time^2);
+g = 9.81;
+L = g / (omega_d^2);
+
+delta = mean(log(pks(1:end-1)./pks(2:end)));
+zeta = delta / sqrt(4*pi^2 + delta^2);
+
+omega_n = omega_d / sqrt(1 - zeta^2);
 
 figure(5);
 clf(5);
 plot(t_sel,theta_sel), hold on
 plot(locs, interp1(t_sel,theta_sel,locs), 'ro')
-xlabel('Time (s)'), ylabel('Signal'), title(sprintf('Freq ≈ %.3f Hz',fn_time))
+xlabel('Time (s)'), ylabel('Signal'), title(sprintf('Freq ≈ %.3f Hz',omega_d))
 grid on, hold off
 
-disp("natural frequency = " + fn_time + " rad/s")
-disp("length = " + length + " m")
+disp("damped frequency = " + omega_d + " rad/s")
+disp("natural frequency = " + omega_n + " rad/s")
+disp("length = " + L + " m")
+disp("zeta = " + zeta)
+%% 
+A = pks(1);  % initial amplitude
+
+decay = A*exp(-zeta*omega_n*t_sel);
+decay_neg = -decay;
+
+figure(5);
+clf(5)
+
+plot(t_sel, theta_sel, 'b', 'LineWidth',1.2); hold on
+plot(locs, pks, 'ro')
+
+plot(t_sel, decay, 'k--','LineWidth',1.5)
+plot(t_sel, decay_neg, 'k--','LineWidth',1.5)
+
+xlabel('Time (s)')
+ylabel('Theta (rad)')
+title(sprintf('ω_d = %.3f rad/s,  ζ = %.4f', omega_d, zeta))
+grid on
+hold off
+
--- load_pendulum_data.m
+++ load_pendulum_data.m
@@ -4,13 +4,13 @@ function [t, theta] = load_pendulum_data()
     fpath = 'C:\Users\nbanse\OneDrive - Olin College of Engineering\Desktop\ESA\Rocky_VI-VII\'; %path (change this!)
     fname_in = 'pendulum_calibration_data.txt'; %file name (change this!)
     %load the pendulum calibration data
-    pendulum_data = importdata([fpath,fname_in]);
+    pendulum_data = importdata(fname_in);
     %unpack the pendulum calibration data
     t = pendulum_data(:,1); theta = pendulum_data(:,2);
     %plot the motor calibration data
-    figure(1);
-    hold on
-    plot(t,theta,'k','linewidth',1);
-    xlabel('time (sec)'); ylabel('angle (rad)');
-    title('Pendulum Calibration Data');
+    %figure(1);
+    %hold on
+    %plot(t,theta,'k','linewidth',1);
+    %xlabel('time (sec)'); ylabel('angle (rad)');
+    %title('Pendulum Calibration Data');
 end
